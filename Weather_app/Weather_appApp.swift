57ffcd405ab96d48c50596c9a78ee116a6edf069
olivier <olivierjacquet333@gmail.com>
2024-09-28 17:43:06 +0400

    start

---
+++ Weather_app/Weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_appApp.swift
+//  Weather_app
+//
+//  Created by Stellone Olivier on 28/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
